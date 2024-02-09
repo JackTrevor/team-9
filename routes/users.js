3bf3fd029eae966031b11b2399c89044d8e5b5c8
erikfenriz <erikfenriz@gmail.com>
2024-02-09 19:10:54 +0100

    validators

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users');
-const { isAuthenticated } = require('../middleware/authenticate');
-
-
+const {isAuthenticated} = require('../middleware/authenticate');
+const {validations, validate} = require("../middleware/validator");
 
 router.get('/', usersController.getAll);
-
 router.get('/:id', usersController.getSingle);
 
-router.post('/', usersController.createUser);
-
-router.put('/:id', usersController.updateUser);
-
+router.post('/', validations().users, validate, usersController.createUser);
+router.put('/:id', validations().users, validate, usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
-
-
 
 module.exports = router;
